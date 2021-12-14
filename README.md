fc19b5c92c42758918173707f4604f341802aa51
gsrbhamrah <bhamrahrussel@gmail.com>
2021-12-14 13:06:55 -0500

    Added 'NFTFactory.sol', 'run,js'

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# nftfactory-offchaindata
-Standard NFT factory contract template with off-chain metadata storage through IFPS.
+# NFT Factory w/ Off-Chain Metadata
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ contracts/NFTFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol";
+
+contract NFTFactory is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    event CreatedNFT(uint256 indexed tokenId, string tokenURI);
+
+    constructor() ERC721("My NFT Collection", "MNC") {}
+
+    function mintNFT(string memory _tokenURI) public returns (uint256) {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+
+        emit CreatedNFT(newItemId, _tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
